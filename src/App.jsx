4b953c6b8db627b552b9e3b81785df4b6eb0f709
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-15 00:21:55 +0300

    add function that checks and logs the winner

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App(){
     const [turn, setTurn] = React.useState("O")
     const [squares, setSquares] = React.useState(squaresState)
     React.useEffect(() => {
+        analyze()
         switchTurn()
     }, [squares])
 
@@ -33,13 +34,36 @@ export default function App(){
     }
 
     function updateSquare(id){
-        console.log("Square " + id + " is clicked")
         setSquares(prevSquares => prevSquares.map(prevSquare => {
             if( prevSquare.id === id && prevSquare.clickedBy === undefined) 
                 return {...prevSquare, clickedBy: turn}
             else
                 return prevSquare
         }))
+    }
+
+    function analyze(){
+        for (let i = 0; i < 3; i++) {
+            let set = new Set([squares[3*i].clickedBy ?? "a", squares[3*i+1].clickedBy, squares[3*i+2].clickedBy])
+            if (set.size === 1){
+                console.log(squares[3*i].clickedBy + " won by Row")
+                return
+            }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            let set = new Set([squares[i].clickedBy ?? "a", squares[i+3].clickedBy, squares[i+6].clickedBy])
+            if (set.size === 1){
+                console.log(squares[i].clickedBy + " won by Column")
+                return
+            }
+        }
+
+        let set = new Set([squares[0].clickedBy ?? "a", squares[4].clickedBy, squares[8].clickedBy])
+        let set2 = new Set([squares[2].clickedBy ?? "a", squares[4].clickedBy, squares[6].clickedBy])
+        if(set.size === 1 || set2.size === 1){
+            console.log(squares[4].clickedBy + " won by Diagonal")
+        }
     }
 
     return (
