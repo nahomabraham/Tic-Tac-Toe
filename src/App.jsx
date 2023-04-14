aad803209159e4fed6fc57fd206c4b3782b540d1
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-14 21:20:30 +0300

    Add styling to the main body

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Square from "./Square"
 
 export default function App(){
     return (
         <main>
-            Hello World
+            <Square />
+            <Square />
+            <Square />
         </main>
     )
 }
