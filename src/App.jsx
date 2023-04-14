f02ef30c4f1dd2e91055d59d42432340a82d06a0
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-14 23:46:07 +0300

    add states to display x and o on click

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,49 @@ import React from "react";
 import Square from "./Square"
 
 export default function App(){
+    let squaresState = []
+    React.useEffect(() => {
+        
+    },[])
+    for (let i = 0; i < 9; i++) {
+        squaresState.push({
+            clickedBy: undefined,
+            id: `${i}`
+        })
+    }
+    const [turn, setTurn] = React.useState("O")
+    const [squares, setSquares] = React.useState(squaresState)
+    React.useEffect(() => {
+        switchTurn()
+    }, [squares])
+
+    let squareElements = []
+    for (let i = 0; i < 9; i++) {
+        squareElements.push(<Square
+            key={`${i}`}
+            square={squares[i]}
+            updateSquare={() => updateSquare(`${i}`)}/>)
+    }
+
+    function switchTurn(){
+        setTurn(prevTurn => (
+            prevTurn === "O" ? "X" : "O"
+        ))
+    }
+
+    function updateSquare(id){
+        console.log("Square " + id + " is clicked")
+        setSquares(prevSquares => prevSquares.map(prevSquare => {
+            if( prevSquare.id === id && prevSquare.clickedBy === undefined) 
+                return {...prevSquare, clickedBy: turn}
+            else
+                return prevSquare
+        }))
+    }
+
     return (
         <main>
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
+            {squareElements}
         </main>
     )
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-export default function Square(){
+export default function Square(props){
+    let className;
+    if (props.square.clickedBy === "O")
+        className = "square-o"
+    else if (props.square.clickedBy === "X")
+        className = "square-x"
+    else
+        className = "square"
+    let update = props.square.clickedBy === undefined
     return (
-        <div className="square">
+        <div className={className} onClick = {update ? props.updateSquare : undefined}>
             
         </div>
     )
