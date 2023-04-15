fc197d979034d77fac04f663173cc789c14938cb
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-15 22:05:46 +0300

    change color of the line when someone wins

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import initState from "../Model/initState.json"
 export default function App(){
     const [turn, setTurn] = React.useState("O")
     const [squares, setSquares] = React.useState(initState)
-    
+    const [gameOver, setGameOver] = React.useState(false)
+    const [winner, setWinner] = React.useState(undefined)
+
     React.useEffect(() => {
-        analyze()
+        setWinner(analyze())
+        console.log(winner)
         switchTurn()
     }, [squares])
 
+    React.useEffect(() => {
+        if (!gameOver){
+            setSquares(initState)
+        }
+    }, [gameOver])
     let squareElements = []
     for (let i = 0; i < 9; i++) {
         squareElements.push(<Square
             key={`${i}`}
+            id={`${i}`}
             square={squares[i]}
-            updateSquare={() => updateSquare(`${i}`)}/>)
+            updateSquare={() => updateSquare(`${i}`)}
+            gameOver={gameOver}
+            winner={winner}/>)
     }
 
     function switchTurn(){
@@ -26,6 +37,10 @@ export default function App(){
     }
 
     function updateSquare(id){
+        if (gameOver){
+            setGameOver(false)
+            return
+        }
         setSquares(prevSquares => prevSquares.map(prevSquare => {
             if( prevSquare.id === id && prevSquare.clickedBy === undefined) 
                 return {...prevSquare, clickedBy: turn}
@@ -39,7 +54,8 @@ export default function App(){
             let set = new Set([squares[3*i].clickedBy ?? "a", squares[3*i+1].clickedBy, squares[3*i+2].clickedBy])
             if (set.size === 1){
                 console.log(squares[3*i].clickedBy + " won by Row")
-                return
+                setGameOver(true)
+                return [squares[3*i].clickedBy, 3*i, 3*i+1, 3*i+2]
             }
         }
 
@@ -47,14 +63,22 @@ export default function App(){
             let set = new Set([squares[i].clickedBy ?? "a", squares[i+3].clickedBy, squares[i+6].clickedBy])
             if (set.size === 1){
                 console.log(squares[i].clickedBy + " won by Column")
-                return
+                setGameOver(true)
+                return [squares[i].clickedBy, i, i+3, i+6]
             }
         }
 
         let set = new Set([squares[0].clickedBy ?? "a", squares[4].clickedBy, squares[8].clickedBy])
         let set2 = new Set([squares[2].clickedBy ?? "a", squares[4].clickedBy, squares[6].clickedBy])
-        if(set.size === 1 || set2.size === 1){
+        if(set.size === 1){
             console.log(squares[4].clickedBy + " won by Diagonal")
+            setGameOver(true)
+            return [squares[4].clickedBy, 0, 4, 8]
+        }
+        if(set2.size === 1){
+            console.log(squares[4].clickedBy + " won by Diagonal")
+            setGameOver(true)
+            return [squares[4].clickedBy, 2, 4, 6]
         }
     }
 
--- src/Square.jsx
+++ src/Square.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 export default function Square(props){
     let className;
-    if (props.square.clickedBy === "O")
+
+    if (props.gameOver && (props.winner[1] == props.id || props.winner[2] == props.id || props.winner[3] == props.id))
+        className = `square-${props.winner[0].toLowerCase()}-wins`
+    else if (props.square.clickedBy === "O")
         className = "square-o"
     else if (props.square.clickedBy === "X")
         className = "square-x"
-    else
+    else {
         className = "square"
+    }
     let shouldUpdate = props.square.clickedBy === undefined
     return (
         <div className={className} onClick = {shouldUpdate ? props.updateSquare : undefined}>
